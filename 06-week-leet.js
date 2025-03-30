bbb7ac1e8b803a5f357ae613df5fc72747ec8e52
XenaSit <sitnikovaxena@gmail.com>
2025-03-30 06:17:41 -0400

    LEET, branch:leet, the DAILY challenge: 763. Partition Labels

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -23087,8 +23087,29 @@ console.log("==========================================")
 // @return {number[]}
 
 var partitionLabels = function(s) {
+    let lastIndex = {};
     
+    // Store the last occurrence of each character
+    for (let i = 0; i < s.length; i++) {
+        lastIndex[s[i]] = i;
+    }
+    
+    let result = [];
+    let start = 0, end = 0;
+    
+    // Traverse the string and determine partition sizes
+    for (let i = 0; i < s.length; i++) {
+        end = Math.max(end, lastIndex[s[i]]);
+        
+        if (i === end) {
+            result.push(end - start + 1);
+            start = i + 1;
+        }
+    }
+    
+    return result;
 };
+
 
 console.log("==========================================")
 // console.log("==========================================")
