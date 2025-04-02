66e781b3eac3b888c80eac871e18e205a91b9729
XenaSit <sitnikovaxena@gmail.com>
2025-04-02 19:57:31 -0400

    LEET, branch:leet, the DAILY challenge: 1. Two Sum

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -23237,7 +23237,19 @@ console.log("==========================================")
 // @return {number[]}
 
 var twoSum = function(nums, target) {
+    let map = new Map();
     
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        
+        map.set(nums[i], i);
+    }
+    
+    return []; // This case won't happen as per the problem statement.
 };
 
 console.log("==========================================")
