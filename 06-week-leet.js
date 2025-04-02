34ec2751e327a85c89c293029294d26307077ce0
XenaSit <sitnikovaxena@gmail.com>
2025-04-02 19:56:15 -0400

    LEET, branch:leet, the DAILY challenge: 1. Two Sum

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -23232,7 +23232,13 @@ Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
  
+@param {number[]} nums
+@param {number} target
+@return {number[]}
 
+var twoSum = function(nums, target) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
