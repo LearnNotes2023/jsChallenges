1c8975d10837c0f53ad9520c1ffcacb991eff702
XenaSit <sitnikovaxena@gmail.com>
2025-05-04 16:02:59 -0400

    LEET, branch:leet, the DAILY challenge: 1128. Number of Equivalent Domino Pairs

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -25145,22 +25145,22 @@ var minDominoRotations = function(tops, bottoms) {
 
 console.log("==========================================")
 
-1128. Number of Equivalent Domino Pairs
-Easy
-Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), 
-or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
-Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
+// 1128. Number of Equivalent Domino Pairs
+// Easy
+// Given a list of dominoes, dominoes[i] = [a, b] is equivalent to dominoes[j] = [c, d] if and only if either (a == c and b == d), 
+// or (a == d and b == c) - that is, one domino can be rotated to be equal to another domino.
+// Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and dominoes[i] is equivalent to dominoes[j].
 
-Example 1:
-Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
-Output: 1
+// Example 1:
+// Input: dominoes = [[1,2],[2,1],[3,4],[5,6]]
+// Output: 1
 
-Example 2:
-Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
-Output: 3
+// Example 2:
+// Input: dominoes = [[1,2],[1,2],[1,1],[1,2],[2,2]]
+// Output: 3
  
-@param {number[][]} dominoes
-@return {number}
+// @param {number[][]} dominoes
+// @return {number}
 
 var numEquivDominoPairs = function(dominoes) {
     
