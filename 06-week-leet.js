87413cddbead86bd486bc3ce5371944e5d4741df
XenaSit <sitnikovaxena@gmail.com>
2025-05-06 08:53:39 -0400

    LEET, branch:leet, the DAILY challenge: 1920. Build Array from Permutation

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -25249,7 +25249,11 @@ console.log("==========================================")
 // @return {number[]}
 
 var buildArray = function(nums) {
-    
+    let ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
 };
 
 console.log("==========================================")
