1ebf2a4bc2e51b3226c746f7e44891e4dc38d325
XenaSit <sitnikovaxena@gmail.com>
2025-05-08 10:03:28 -0400

    LEET, branch:leet, the DAILY challenge: 3342. Find Minimum Time to Reach Last Room II

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -25354,9 +25354,38 @@ console.log("==========================================")
 // @param {number[][]} moveTime
 // @return {number}
 
-var minTimeToReach = function(moveTime) {
-    
-};
+function minTimeToReach(moveTime) {
+    const n = moveTime.length;
+    const m = moveTime[0].length;
+    const dist = Array.from({ length: n }, () => Array(m).fill(Infinity));
+    dist[0][0] = 0;
+  
+    // PriorityQueue class needs to be implemented or imported for this to work in JS
+    const pq = new PriorityQueue((a, b) => a[0] - b[0]);
+    pq.enqueue([0, 0, 0]);
+  
+    const dirs = [-1, 0, 1, 0, -1];  // Directions for moving (up, right, down, left)
+  
+    while (!pq.isEmpty()) {
+      const [d, i, j] = pq.dequeue();
+      if (d > dist[i][j]) continue;
+      if (i === n - 1 && j === m - 1) return d;  // Destination reached
+      
+      for (let k = 0; k < 4; k++) {
+        const x = i + dirs[k];
+        const y = j + dirs[k + 1];
+        if (x >= 0 && x < n && y >= 0 && y < m) {
+          const t = Math.max(moveTime[x][y], d) + ((i + j) % 2) + 1;
+          if (t < dist[x][y]) {
+            dist[x][y] = t;
+            pq.enqueue([t, x, y]);
+          }
+        }
+      }
+    }
+  
+    return -1;  // If no path is found
+  }
 
 console.log("==========================================")
 // console.log("==========================================")
