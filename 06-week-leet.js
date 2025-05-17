4598fb61cf49f347b54f9041e38748cff0928dfa
XenaSit <sitnikovaxena@gmail.com>
2025-05-17 08:10:05 -0400

    LEET, branch:leet, the DAILY challenge: 2677. Chunk Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -25979,7 +25979,11 @@ console.log("==========================================")
 // @return {Array}
 
 var chunk = function(arr, size) {
-    
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size));
+    }
+    return result;
 };
 
 
