a473890e10affb83192b43ac44481d7b4b751f27
XenaSit <sitnikovaxena@gmail.com>
2025-05-21 09:33:43 -0400

    LEET, branch:leet, the DAILY challenge: 73. Set Matrix Zeroes

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -26194,6 +26194,12 @@ Example 2:
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 
+@param {number[][]} matrix
+@return {void} Do not return anything, modify matrix in-place instead.
+
+var setZeroes = function(matrix) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
