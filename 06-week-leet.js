b6a22892dbbe47b3a219124a426269735a43d7f9
XenaSit <sitnikovaxena@gmail.com>
2025-05-23 10:08:59 -0400

    LEET, branch:leet, the DAILY challenge: 2619. Array Prototype Last

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -26357,7 +26357,7 @@ console.log("==========================================")
 // @return {null|boolean|number|string|Array|Object}
 
 Array.prototype.last = function() {
-    
+    return this.length === 0 ? -1 : this[this.length - 1];
 };
 
 // const arr = [1, 2, 3];
