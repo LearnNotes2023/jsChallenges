b11d20f61876b695f62eca96a6313d0c1604b9a1
XenaSit <sitnikovaxena@gmail.com>
2025-05-31 08:49:29 -0400

    LEET, branch:leet, the DAILY challenge: 909. Snakes and Ladders

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -26786,7 +26786,43 @@ console.log("==========================================")
 // @return {number}
 
 var snakesAndLadders = function(board) {
-    
+    const n = board.length;
+
+    // Convert the square number to row/col
+    function getCoordinates(s) {
+        const quot = Math.floor((s - 1) / n);
+        const rem = (s - 1) % n;
+        const row = n - 1 - quot;
+        const col = (quot % 2 === 0) ? rem : (n - 1 - rem);
+        return [row, col];
+    }
+
+    const visited = new Array(n * n + 1).fill(false);
+    const queue = [[1, 0]]; // [square, moves]
+    visited[1] = true;
+
+    while (queue.length > 0) {
+        const [curr, moves] = queue.shift();
+
+        if (curr === n * n) return moves;
+
+        for (let i = 1; i <= 6; i++) {
+            let next = curr + i;
+            if (next > n * n) break;
+
+            const [r, c] = getCoordinates(next);
+            if (board[r][c] !== -1) {
+                next = board[r][c];
+            }
+
+            if (!visited[next]) {
+                visited[next] = true;
+                queue.push([next, moves + 1]);
+            }
+        }
+    }
+
+    return -1;
 };
 
 console.log("==========================================")
