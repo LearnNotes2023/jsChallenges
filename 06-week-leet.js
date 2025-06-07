d1f3a4b730cc019ef07737bd3e2bfce4090b9127
XenaSit <sitnikovaxena@gmail.com>
2025-06-07 09:41:21 -0400

    LEET, branch:leet, the DAILY challenge: 3170. Lexicographically Minimum String After Removing Stars

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -27229,9 +27229,30 @@ console.log("==========================================")
 // @param {string} s
 // @return {string}
 
-var clearStars = function(s) {
-    
-};
+function clearStars(s) {
+    const g = Array.from({ length: 26 }, () => []);
+    const n = s.length;
+    const rem = Array(n).fill(false);
+
+    for (let i = 0; i < n; ++i) {
+        if (s[i] === '*') {
+            rem[i] = true;
+            for (let j = 0; j < 26; ++j) {
+                if (g[j].length) {
+                    rem[g[j].pop()] = true;
+                    break;
+                }
+            }
+        } else {
+            g[s.charCodeAt(i) - 97].push(i);
+        }
+    }
+
+    return s
+        .split('')
+        .filter((_, i) => !rem[i])
+        .join('');
+}
 
 console.log("==========================================")
 // console.log("==========================================")
