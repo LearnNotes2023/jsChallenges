ac4720003dcc226a0f7cb23bdea228e9434fb5a8
XenaSit <sitnikovaxena@gmail.com>
2025-06-27 11:32:09 -0400

    LEET, branch:leet, the DAILY challenge: 2014. Longest Subsequence Repeated k Times

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -28361,35 +28361,35 @@ var longestSubsequence = function(s, k) {
 
 console.log("==========================================")
 
-2014. Longest Subsequence Repeated k Times
-Hard
-You are given a string s of length n, and an integer k. You are tasked to find the longest subsequence repeated k times in string s.
-A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
-A subsequence seq is repeated k times in the string s if seq * k is a subsequence of s, where seq * k represents a string constructed by concatenating seq k times.
-For example, "bba" is repeated 2 times in the string "bababcba", because the string "bbabba", constructed by concatenating "bba" 2 times, is a subsequence of the string "bababcba".
-Return the longest subsequence repeated k times in string s. If multiple such subsequences are found, return the lexicographically largest one. 
-If there is no such subsequence, return an empty string.
+// 2014. Longest Subsequence Repeated k Times
+// Hard
+// You are given a string s of length n, and an integer k. You are tasked to find the longest subsequence repeated k times in string s.
+// A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+// A subsequence seq is repeated k times in the string s if seq * k is a subsequence of s, where seq * k represents a string constructed by concatenating seq k times.
+// For example, "bba" is repeated 2 times in the string "bababcba", because the string "bbabba", constructed by concatenating "bba" 2 times, is a subsequence of the string "bababcba".
+// Return the longest subsequence repeated k times in string s. If multiple such subsequences are found, return the lexicographically largest one. 
+// If there is no such subsequence, return an empty string.
 
-Example 1:
-example 1
-Input: s = "letsleetcode", k = 2
-Output: "let"
-Explanation: There are two longest subsequences repeated 2 times: "let" and "ete".
-"let" is the lexicographically largest one.
+// Example 1:
+// example 1
+// Input: s = "letsleetcode", k = 2
+// Output: "let"
+// Explanation: There are two longest subsequences repeated 2 times: "let" and "ete".
+// "let" is the lexicographically largest one.
 
-Example 2:
-Input: s = "bb", k = 2
-Output: "b"
-Explanation: The longest subsequence repeated 2 times is "b".
+// Example 2:
+// Input: s = "bb", k = 2
+// Output: "b"
+// Explanation: The longest subsequence repeated 2 times is "b".
 
-Example 3:
-Input: s = "ab", k = 2
-Output: ""
-Explanation: There is no subsequence repeated 2 times. Empty string is returned.
+// Example 3:
+// Input: s = "ab", k = 2
+// Output: ""
+// Explanation: There is no subsequence repeated 2 times. Empty string is returned.
 
-@param {string} s
-@param {number} k
-@return {string}
+// @param {string} s
+// @param {number} k
+// @return {string}
 
 var longestSubsequenceRepeatedK = function(s, k) {
     
