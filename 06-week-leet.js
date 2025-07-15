4baf495afb44c73056613477d00b5fe8dc5f67a1
XenaSit <sitnikovaxena@gmail.com>
2025-07-15 09:22:35 -0400

    LEET, branch:leet, the DAILY challenge: 3136. Valid Word

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -29553,7 +29553,26 @@ console.log("==========================================")
 // @return {boolean}
 
 var isValid = function(word) {
-    
+    if (word.length < 3) return false;
+
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let hasVowel = false;
+    let hasConsonant = false;
+
+    for (let char of word) {
+        if (!/[a-zA-Z0-9]/.test(char)) return false;
+
+        if (/[a-zA-Z]/.test(char)) {
+            const lowerChar = char.toLowerCase();
+            if (vowels.has(lowerChar)) {
+                hasVowel = true;
+            } else {
+                hasConsonant = true;
+            }
+        }
+    }
+
+    return hasVowel && hasConsonant;
 };
 
 console.log("==========================================")
