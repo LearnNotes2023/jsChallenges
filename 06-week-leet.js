38ee53ba7ca603b7b93badb4fbe5ae762e2d5c92
XenaSit <sitnikovaxena@gmail.com>
2025-07-30 09:17:32 -0400

    LEET, branch:leet, the DAILY challenge: 2419. Longest Subarray With Maximum Bitwise AND

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -30603,7 +30603,12 @@ Explanation:
 The maximum possible bitwise AND of a subarray is 4.
 The longest subarray with that value is [4], so we return 1.
 
+@param {number[]} nums
+@return {number}
 
+var longestSubarray = function(nums) {
+    
+};
 
 
 
