6729672b37173afbed50f560f094cdb45b093463
XenaSit <sitnikovaxena@gmail.com>
2025-07-30 09:18:51 -0400

    LEET, branch:leet, the DAILY challenge: 2419. Longest Subarray With Maximum Bitwise AND

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -30607,7 +30607,20 @@ console.log("==========================================")
 // @return {number}
 
 var longestSubarray = function(nums) {
-    
+    let maxVal = Math.max(...nums);
+    let maxLength = 0;
+    let currentLength = 0;
+
+    for (let num of nums) {
+        if (num === maxVal) {
+            currentLength++;
+            maxLength = Math.max(maxLength, currentLength);
+        } else {
+            currentLength = 0;
+        }
+    }
+
+    return maxLength;
 };
 
 
