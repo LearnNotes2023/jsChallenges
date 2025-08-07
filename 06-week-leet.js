d49d306a6f5deacce6d9e7ac9e62beff07acfa17
XenaSit <sitnikovaxena@gmail.com>
2025-08-07 13:35:36 -0400

    LEET, branch:leet, the DAILY challenge: 3363. Find the Maximum Number of Fruits Collected

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31060,9 +31060,57 @@ console.log("==========================================")
 // @param {number[][]} fruits
 // @return {number}
 
-var maxCollectedFruits = function(fruits) {
-    
-};
+function maxCollectedFruits(fruits) {
+  const n = fruits.length;
+
+  function getTopLeft() {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+      sum += fruits[i][i];
+    }
+    return sum;
+  }
+
+  function getTopRight() {
+    const dp = Array.from({ length: n }, () => Array(n).fill(0));
+    dp[0][n - 1] = fruits[0][n - 1];
+
+    for (let x = 0; x < n; x++) {
+      for (let y = 0; y < n; y++) {
+        if (x >= y && !(x === n - 1 && y === n - 1)) continue;
+        for (const [dx, dy] of [[1, -1], [1, 0], [1, 1]]) {
+          const i = x - dx;
+          const j = y - dy;
+          if (i < 0 || i >= n || j < 0 || j >= n) continue;
+          if (i < j && j < n - 1 - i) continue;
+          dp[x][y] = Math.max(dp[x][y], dp[i][j] + fruits[x][y]);
+        }
+      }
+    }
+    return dp[n - 1][n - 1];
+  }
+
+  function getBottomLeft() {
+    const dp = Array.from({ length: n }, () => Array(n).fill(0));
+    dp[n - 1][0] = fruits[n - 1][0];
+
+    for (let y = 0; y < n; y++) {
+      for (let x = 0; x < n; x++) {
+        if (x <= y && !(x === n - 1 && y === n - 1)) continue;
+        for (const [dx, dy] of [[-1, 1], [0, 1], [1, 1]]) {
+          const i = x - dx;
+          const j = y - dy;
+          if (i < 0 || i >= n || j < 0 || j >= n) continue;
+          if (j < i && i < n - 1 - j) continue;
+          dp[x][y] = Math.max(dp[x][y], dp[i][j] + fruits[x][y]);
+        }
+      }
+    }
+    return dp[n - 1][n - 1];
+  }
+
+  return getTopLeft() + getTopRight() + getBottomLeft() - 2 * fruits[n - 1][n - 1];
+}
 
 
 console.log("==========================================")
