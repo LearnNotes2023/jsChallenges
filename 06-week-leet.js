77480d19d6f91954a31faa4d6220c1f9652555b7
XenaSit <sitnikovaxena@gmail.com>
2025-08-10 11:13:10 -0400

    LEET, branch:leet, the DAILY challenge: 869. Reordered Power of 2

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31223,21 +31223,21 @@ var isPowerOfTwo = function(n) {
 
 console.log("==========================================")
 
-869. Reordered Power of 2
-Medium
-You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
-Return true if and only if we can do this so that the resulting number is a power of two.
+// 869. Reordered Power of 2
+// Medium
+// You are given an integer n. We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+// Return true if and only if we can do this so that the resulting number is a power of two.
 
-Example 1:
-Input: n = 1
-Output: true
+// Example 1:
+// Input: n = 1
+// Output: true
 
-Example 2:
-Input: n = 10
-Output: false
+// Example 2:
+// Input: n = 10
+// Output: false
 
-@param {number} n
-@return {boolean}
+// @param {number} n
+// @return {boolean}
 
 var reorderedPowerOf2 = function(n) {
     
