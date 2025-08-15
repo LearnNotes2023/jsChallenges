711781de38adaecdbf33732f1d7d9da1b1f96afa
XenaSit <sitnikovaxena@gmail.com>
2025-08-15 06:52:47 -0400

    LEET, branch:leet, the DAILY challenge: 342. Power of Four

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31489,7 +31489,7 @@ console.log("==========================================")
 // @return {boolean}
 
 var isPowerOfFour = function(n) {
-    
+    return n > 0 && (Math.log(n) / Math.log(4)) % 1 === 0;
 };
 
 
