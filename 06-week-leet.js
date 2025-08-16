6013ed23b6f7801556fb4c0b81e28bc060d783bf
XenaSit <sitnikovaxena@gmail.com>
2025-08-16 09:08:58 -0400

    LEET, branch:leet, the DAILY challenge: 1323. Maximum 69 Number

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31523,10 +31523,21 @@ console.log("==========================================")
 // @param {number} num
 // @return {number}
 
-var maximum69Number  = function(num) {
+var maximum69Number = function(num) {
+    // Convert number to string for easy manipulation
+    let str = num.toString().split("");
     
+    // Find the first '6' and change it to '9'
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === '6') {
+            str[i] = '9';
+            break; // only change once
+        }
+    }
+    
+    // Convert back to number
+    return parseInt(str.join(""));
 };
-
 
 console.log("==========================================")
 // console.log("==========================================")
