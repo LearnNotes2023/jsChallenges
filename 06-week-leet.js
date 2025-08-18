fc775be62b0d2b1c22cdd43d8e618dee8d2d5926
XenaSit <sitnikovaxena@gmail.com>
2025-08-18 12:00:14 -0400

    LEET, branch:leet, the DAILY challenge: 679. 24 Game

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31624,7 +31624,45 @@ console.log("==========================================")
 // @return {boolean}
 
 var judgePoint24 = function(cards) {
-    
+    const EPS = 1e-6;
+
+    function backtrack(nums) {
+        if (nums.length === 1) {
+            return Math.abs(nums[0] - 24) < EPS;
+        }
+
+        // try all pairs of numbers
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (i === j) continue;
+
+                // new array without nums[i] and nums[j]
+                let next = [];
+                for (let k = 0; k < nums.length; k++) {
+                    if (k !== i && k !== j) {
+                        next.push(nums[k]);
+                    }
+                }
+
+                // try all possible results of combining nums[i] and nums[j]
+                for (let val of compute(nums[i], nums[j])) {
+                    next.push(val);
+                    if (backtrack(next)) return true;
+                    next.pop(); // backtrack
+                }
+            }
+        }
+        return false;
+    }
+
+    function compute(a, b) {
+        let results = [a + b, a - b, b - a, a * b];
+        if (Math.abs(b) > 1e-6) results.push(a / b);
+        if (Math.abs(a) > 1e-6) results.push(b / a);
+        return results;
+    }
+
+    return backtrack(cards.map(x => x * 1.0)); // convert to float
 };
 
 console.log("==========================================")
