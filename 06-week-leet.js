81a0002dbf7cfb86e4df6e33a38edec5ace76ebc
XenaSit <sitnikovaxena@gmail.com>
2025-08-22 05:46:24 -0400

    LEET, branch:leet, the DAILY challenge: 3195. Find the Minimum Area to Cover All Ones I

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -31833,26 +31833,26 @@ var numSubmat = function(mat) {
 
 console.log("==========================================")
 
-3195. Find the Minimum Area to Cover All Ones I
-Medium
-You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, 
-such that all the 1's in grid lie inside this rectangle.
-Return the minimum possible area of the rectangle.
+// 3195. Find the Minimum Area to Cover All Ones I
+// Medium
+// You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, 
+// such that all the 1's in grid lie inside this rectangle.
+// Return the minimum possible area of the rectangle.
 
-Example 1:
-Input: grid = [[0,1,0],[1,0,1]]
-Output: 6
-Explanation:
-The smallest rectangle has a height of 2 and a width of 3, so it has an area of 2 * 3 = 6.
+// Example 1:
+// Input: grid = [[0,1,0],[1,0,1]]
+// Output: 6
+// Explanation:
+// The smallest rectangle has a height of 2 and a width of 3, so it has an area of 2 * 3 = 6.
 
-Example 2:
-Input: grid = [[1,0],[0,0]]
-Output: 1
-Explanation:
-The smallest rectangle has both height and width 1, so its area is 1 * 1 = 1.
+// Example 2:
+// Input: grid = [[1,0],[0,0]]
+// Output: 1
+// Explanation:
+// The smallest rectangle has both height and width 1, so its area is 1 * 1 = 1.
 
-@param {number[][]} grid
-@return {number}
+// @param {number[][]} grid
+// @return {number}
 
 var minimumArea = function(grid) {
     
