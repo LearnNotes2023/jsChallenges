99b142a94ffac5ff77bbdbdd91ffb802b36f0e15
XenaSit <sitnikovaxena@gmail.com>
2025-09-07 06:21:59 -0400

    LEET, branch:leet, the DAILY challenge: 1304. Find N Unique Integers Sum up to Zero

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -32958,10 +32958,15 @@ console.log("==========================================")
 // @return {number[]}
 
 var sumZero = function(n) {
-    
+    let res = [];
+    for (let i = 1; i <= Math.floor(n / 2); i++) {
+        res.push(i, -i);
+    }
+    if (n % 2 !== 0) {
+        res.push(0);
+    }
+    return res;
 };
-
-
 
 console.log("==========================================")
 // console.log("==========================================")
