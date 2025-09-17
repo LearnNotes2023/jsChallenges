346750646c5a204931a762e8244ba96c714fb934
XenaSit <sitnikovaxena@gmail.com>
2025-09-17 05:58:18 -0400

    LEET, branch:leet, the DAILY challenge: 2353. Design a Food Rating System

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -33521,7 +33521,38 @@ foodRatings.highestRated("japanese"); // return "ramen"
                                       // Both "sushi" and "ramen" have a rating of 16.
                                       // However, "ramen" is lexicographically smaller than "sushi".
 
+/**
+ * @param {string[]} foods
+ * @param {string[]} cuisines
+ * @param {number[]} ratings
+ */
+var FoodRatings = function(foods, cuisines, ratings) {
+    
+};
 
+/** 
+ * @param {string} food 
+ * @param {number} newRating
+ * @return {void}
+ */
+FoodRatings.prototype.changeRating = function(food, newRating) {
+    
+};
+
+/** 
+ * @param {string} cuisine
+ * @return {string}
+ */
+FoodRatings.prototype.highestRated = function(cuisine) {
+    
+};
+
+/** 
+ * Your FoodRatings object will be instantiated and called as such:
+ * var obj = new FoodRatings(foods, cuisines, ratings)
+ * obj.changeRating(food,newRating)
+ * var param_2 = obj.highestRated(cuisine)
+ */
 
 
 
