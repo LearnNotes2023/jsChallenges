8b2a9cc4a800be5067651fef6765ac61478d1ef3
XenaSit <sitnikovaxena@gmail.com>
2025-09-17 06:01:09 -0400

    LEET, branch:leet, the DAILY challenge: 2353. Design a Food Rating System

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -33483,83 +33483,57 @@ var replaceNonCoprimes = function(nums) {
 
 console.log("==========================================")
 
-2353. Design a Food Rating System
-Medium
-Design a food rating system that can do the following:
-Modify the rating of a food item listed in the system.
-Return the highest-rated food item for a type of cuisine in the system.
-Implement the FoodRatings class:
-FoodRatings(String[] foods, String[] cuisines, int[] ratings) Initializes the system. 
-The food items are described by foods, cuisines and ratings, all of which have a length of n.
-foods[i] is the name of the ith food,
-cuisines[i] is the type of cuisine of the ith food, and
-ratings[i] is the initial rating of the ith food.
-void changeRating(String food, int newRating) Changes the rating of the food item with the name food.
-String highestRated(String cuisine) Returns the name of the food item that has the highest rating for the given type of cuisine. 
-If there is a tie, return the item with the lexicographically smaller name.
-Note that a string x is lexicographically smaller than string y if x comes before y in dictionary order, 
-that is, either x is a prefix of y, or if i is the first position such that x[i] != y[i], then x[i] comes before y[i] in alphabetic order.
+// 2353. Design a Food Rating System
+// Medium
+// Design a food rating system that can do the following:
+// Modify the rating of a food item listed in the system.
+// Return the highest-rated food item for a type of cuisine in the system.
+// Implement the FoodRatings class:
+// FoodRatings(String[] foods, String[] cuisines, int[] ratings) Initializes the system. 
+// The food items are described by foods, cuisines and ratings, all of which have a length of n.
+// foods[i] is the name of the ith food,
+// cuisines[i] is the type of cuisine of the ith food, and
+// ratings[i] is the initial rating of the ith food.
+// void changeRating(String food, int newRating) Changes the rating of the food item with the name food.
+// String highestRated(String cuisine) Returns the name of the food item that has the highest rating for the given type of cuisine. 
+// If there is a tie, return the item with the lexicographically smaller name.
+// Note that a string x is lexicographically smaller than string y if x comes before y in dictionary order, 
+// that is, either x is a prefix of y, or if i is the first position such that x[i] != y[i], then x[i] comes before y[i] in alphabetic order.
 
-Example 1:
-Input
-["FoodRatings", "highestRated", "highestRated", "changeRating", "highestRated", "changeRating", "highestRated"]
-[[["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], 
-[9, 12, 8, 15, 14, 7]], ["korean"], ["japanese"], ["sushi", 16], ["japanese"], ["ramen", 16], ["japanese"]]
-Output
-[null, "kimchi", "ramen", null, "sushi", null, "ramen"]
-Explanation
-FoodRatings foodRatings = new FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7]);
-foodRatings.highestRated("korean"); // return "kimchi"
-                                    // "kimchi" is the highest rated korean food with a rating of 9.
-foodRatings.highestRated("japanese"); // return "ramen"
-                                      // "ramen" is the highest rated japanese food with a rating of 14.
-foodRatings.changeRating("sushi", 16); // "sushi" now has a rating of 16.
-foodRatings.highestRated("japanese"); // return "sushi"
-                                      // "sushi" is the highest rated japanese food with a rating of 16.
-foodRatings.changeRating("ramen", 16); // "ramen" now has a rating of 16.
-foodRatings.highestRated("japanese"); // return "ramen"
-                                      // Both "sushi" and "ramen" have a rating of 16.
-                                      // However, "ramen" is lexicographically smaller than "sushi".
+// Example 1:
+// Input
+// ["FoodRatings", "highestRated", "highestRated", "changeRating", "highestRated", "changeRating", "highestRated"]
+// [[["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], 
+// [9, 12, 8, 15, 14, 7]], ["korean"], ["japanese"], ["sushi", 16], ["japanese"], ["ramen", 16], ["japanese"]]
+// Output
+// [null, "kimchi", "ramen", null, "sushi", null, "ramen"]
+// Explanation
+// FoodRatings foodRatings = new FoodRatings(["kimchi", "miso", "sushi", "moussaka", "ramen", "bulgogi"], ["korean", "japanese", "japanese", "greek", "japanese", "korean"], [9, 12, 8, 15, 14, 7]);
+// foodRatings.highestRated("korean"); // return "kimchi"
+//                                     // "kimchi" is the highest rated korean food with a rating of 9.
+// foodRatings.highestRated("japanese"); // return "ramen"
+//                                       // "ramen" is the highest rated japanese food with a rating of 14.
+// foodRatings.changeRating("sushi", 16); // "sushi" now has a rating of 16.
+// foodRatings.highestRated("japanese"); // return "sushi"
+//                                       // "sushi" is the highest rated japanese food with a rating of 16.
+// foodRatings.changeRating("ramen", 16); // "ramen" now has a rating of 16.
+// foodRatings.highestRated("japanese"); // return "ramen"
+//                                       // Both "sushi" and "ramen" have a rating of 16.
+//                                       // However, "ramen" is lexicographically smaller than "sushi".
 
-/**
- * @param {string[]} foods
- * @param {string[]} cuisines
- * @param {number[]} ratings
- */
-var FoodRatings = function(foods, cuisines, ratings) {
-    
-};
 
-/** 
- * @param {string} food 
- * @param {number} newRating
- * @return {void}
- */
-FoodRatings.prototype.changeRating = function(food, newRating) {
-    
-};
+// Your FoodRatings object will be instantiated and called as such:
+// var obj = new FoodRatings(foods, cuisines, ratings)
+// obj.changeRating(food,newRating)
+// var param_2 = obj.highestRated(cuisine)
 
-/** 
- * @param {string} cuisine
- * @return {string}
- */
-FoodRatings.prototype.highestRated = function(cuisine) {
-    
-};
-
-/** 
- * Your FoodRatings object will be instantiated and called as such:
- * var obj = new FoodRatings(foods, cuisines, ratings)
- * obj.changeRating(food,newRating)
- * var param_2 = obj.highestRated(cuisine)
- */
 
 class FoodRatings {
-    /**
-     * @param {string[]} foods
-     * @param {string[]} cuisines
-     * @param {number[]} ratings
-     */
+    
+    // @param {string[]} foods
+    // @param {string[]} cuisines
+    // @param {number[]} ratings
+    
     constructor(foods, cuisines, ratings) {
         this.foodToData = new Map(); // food -> {cuisine, rating}
         this.cuisineToHeap = new Map(); // cuisine -> MaxHeap [ -rating, food ]
@@ -33584,21 +33558,19 @@ class FoodRatings {
         }
     }
 
-    /** 
-     * @param {string} food 
-     * @param {number} newRating
-     * @return {void}
-     */
+    // @param {string} food 
+    // @param {number} newRating
+    // @return {void}
+    
     changeRating(food, newRating) {
         let { cuisine } = this.foodToData.get(food);
         this.foodToData.set(food, { cuisine, rating: newRating });
         this.cuisineToHeap.get(cuisine).enqueue([-newRating, food]);
     }
 
-    /** 
-     * @param {string} cuisine
-     * @return {string}
-     */
+    // @param {string} cuisine
+    // @return {string}
+    
     highestRated(cuisine) {
         let heap = this.cuisineToHeap.get(cuisine);
         
