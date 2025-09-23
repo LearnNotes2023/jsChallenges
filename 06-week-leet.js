12d4635cb83c9b4012f9efa9aeebc9e23d22e944
XenaSit <sitnikovaxena@gmail.com>
2025-09-23 05:47:49 -0400

    LEET, branch:leet, the DAILY challenge: 2726. Calculator with Method Chaining

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -34217,7 +34217,62 @@ Explanation:
 new Calculator(20).divide(0).getResult() // 20 / 0 
 The error should be thrown because we cannot divide by zero.
 
-
+class Calculator {
+    
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        
+    }
+}
 
 
 console.log("==========================================")
