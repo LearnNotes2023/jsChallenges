1244c945eada5ee3cd20f3ad9765a9f0156714fc
XenaSit <sitnikovaxena@gmail.com>
2025-09-24 05:41:33 -0400

    LEET, branch:leet, the DAILY challenge: 166. Fraction to Recurring Decimal

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -34264,25 +34264,50 @@ class Calculator {
         return this;
     }
     
-    /** 
-     * @param {number} value
-     * @return {Calculator}
-     */
+    // @param {number} value
+    // @return {Calculator}
+    
     power(value) {
         this.result = Math.pow(this.result, value);
         return this;
     }
     
-    /** 
-     * @return {number}
-     */
+    
+    // @return {number}
+    
     getResult() {
         return this.result;
     }
 }
 
 console.log("==========================================")
-// console.log("==========================================")
+
+166. Fraction to Recurring Decimal
+Medium
+Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+If the fractional part is repeating, enclose the repeating part in parentheses.
+If multiple answers are possible, return any of them.
+It is guaranteed that the length of the answer string is less than 104 for all the given inputs.
+
+Example 1:
+Input: numerator = 1, denominator = 2
+Output: "0.5"
+
+Example 2:
+Input: numerator = 2, denominator = 1
+Output: "2"
+
+Example 3:
+Input: numerator = 4, denominator = 333
+Output: "0.(012)"
+
+
+
+
+
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
