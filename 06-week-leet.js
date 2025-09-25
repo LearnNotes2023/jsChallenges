035d5f75ed2d9247c3050b8c5e2d39131b73ec0a
XenaSit <sitnikovaxena@gmail.com>
2025-09-25 06:48:09 -0400

    LEET, branch:leet, the DAILY challenge: 120. Triangle

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -34351,31 +34351,39 @@ var fractionToDecimal = function(numerator, denominator) {
 
 console.log("==========================================")
 
-120. Triangle
-Medium
-Given a triangle array, return the minimum path sum from top to bottom.
-For each step, you may move to an adjacent number of the row below. 
-More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+// 120. Triangle
+// Medium
+// Given a triangle array, return the minimum path sum from top to bottom.
+// For each step, you may move to an adjacent number of the row below. 
+// More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 
-Example 1:
-Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
-Output: 11
-Explanation: The triangle looks like:
-   2
-  3 4
- 6 5 7
-4 1 8 3
-The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+// Example 1:
+// Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+// Output: 11
+// Explanation: The triangle looks like:
+//    2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+// The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
 
-Example 2:
-Input: triangle = [[-10]]
-Output: -10
+// Example 2:
+// Input: triangle = [[-10]]
+// Output: -10
  
-@param {number[][]} triangle
-@return {number}
+// @param {number[][]} triangle
+// @return {number}
 
 var minimumTotal = function(triangle) {
-    
+    // Start from the second to last row and move upward
+    for (let row = triangle.length - 2; row >= 0; row--) {
+        for (let col = 0; col < triangle[row].length; col++) {
+            // Update each element to the minimum path sum from this point
+            triangle[row][col] += Math.min(triangle[row + 1][col], triangle[row + 1][col + 1]);
+        }
+    }
+    // The top element now contains the result
+    return triangle[0][0];
 };
 
 console.log("==========================================")
