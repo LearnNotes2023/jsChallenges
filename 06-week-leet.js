715f1e83f35081132d673a200ea1a66387e7737e
XenaSit <sitnikovaxena@gmail.com>
2025-10-10 05:50:12 -0400

    LEET, branch:leet, the DAILY challenge: 3147. Taking Maximum Energy From the Mystic Dungeon

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -35190,7 +35190,17 @@ console.log("==========================================")
 // @return {number}
 
 var maximumEnergy = function(energy, k) {
+    const n = energy.length;
+    const dp = new Array(n).fill(0);
+    let ans = -Infinity;
     
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i] = energy[i];
+        if (i + k < n) dp[i] += dp[i + k];
+        ans = Math.max(ans, dp[i]);
+    }
+    
+    return ans;
 };
 
 
