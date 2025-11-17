9dfa8f1a1e7c01100ab24d8489f2dc470520cd0d
XenaSit <sitnikovaxena@gmail.com>
2025-11-17 04:37:09 -0500

    LEET, branch:leet, the DAILY challenge: 1437. Check If All 1's Are at Least Length K Places Away

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -37658,7 +37658,18 @@ console.log("==========================================")
 // @return {boolean}
 
 var kLengthApart = function(nums, k) {
-    
+    let prev = -1;  // index of previous 1
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            if (prev !== -1 && i - prev - 1 < k) {
+                return false;
+            }
+            prev = i;
+        }
+    }
+
+    return true;
 };
 
 
