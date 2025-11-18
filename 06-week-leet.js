622430f8da201b34c4d8e71d79953bdc189629dd
XenaSit <sitnikovaxena@gmail.com>
2025-11-18 04:49:58 -0500

    LEET, branch:leet, the DAILY challenge: 717. 1-bit and 2-bit Characters

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -37698,7 +37698,15 @@ console.log("==========================================")
 // @return {boolean}
 
 var isOneBitCharacter = function(bits) {
-    
+    let i = 0;
+    while (i < bits.length - 1) {
+        if (bits[i] === 1) {
+            i += 2; // two-bit character
+        } else {
+            i += 1; // one-bit character
+        }
+    }
+    return i === bits.length - 1;
 };
 
 console.log("==========================================")
