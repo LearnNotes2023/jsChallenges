f86cfc703810198e848cf2b84d6654083f65b754
XenaSit <sitnikovaxena@gmail.com>
2025-11-22 05:32:12 -0500

    LEET, branch:leet, the DAILY challenge: 3190. Find Minimum Operations to Make All Elements Divisible by Three

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -37899,9 +37899,12 @@ console.log("==========================================")
 // @return {number}
 
 var minimumOperations = function(nums) {
-    
+    let ops = 0;
+    for (let num of nums) {
+        if (num % 3 !== 0) ops++;
+    }
+    return ops;
 };
-
 
 console.log("==========================================")
 // console.log("==========================================")
