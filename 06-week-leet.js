1f4c19c095a2eb1900c98a62d47d9d80b839cc2b
XenaSit <sitnikovaxena@gmail.com>
2025-12-07 08:18:11 -0500

    LEET, branch:leet, the DAILY challenge: 1523. Count Odd Numbers in an Interval Range

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -38804,7 +38804,7 @@ console.log("==========================================")
 // @return {number}
 
 var countOdds = function(low, high) {
-    
+    return Math.floor((high + 1) / 2) - Math.floor(low / 2);
 };
 
 console.log("==========================================")
