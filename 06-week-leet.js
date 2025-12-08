0fa26045fa57a09dee68f1aaab3de77e1503fe3f
XenaSit <sitnikovaxena@gmail.com>
2025-12-08 05:52:09 -0500

    LEET, branch:leet, the DAILY challenge: 1925. Count Square Sum Triples

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -38828,7 +38828,19 @@ console.log("==========================================")
 // @return {number}
 
 var countTriples = function(n) {
-    
+    let count = 0;
+
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            let cSquared = a * a + b * b;
+            let c = Math.sqrt(cSquared);
+            if (Number.isInteger(c) && c <= n) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 };
 
 console.log("==========================================")
