445f52a1aa30bc0879a3539ff440431e70d30dcb
XenaSit <sitnikovaxena@gmail.com>
2025-12-21 08:31:49 -0500

    LEET, branch:leet, the DAILY challenge: 955. Delete Columns to Make Sorted II

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -39833,7 +39833,38 @@ console.log("==========================================")
 // @return {number}
 
 var minDeletionSize = function(strs) {
-    
+    const n = strs.length;
+    const m = strs[0].length;
+
+    // sorted[i] means strs[i] < strs[i+1] is already confirmed
+    const sorted = Array(n - 1).fill(false);
+    let deletions = 0;
+
+    for (let col = 0; col < m; col++) {
+        let needDelete = false;
+
+        // Check if this column breaks ordering
+        for (let i = 0; i < n - 1; i++) {
+            if (!sorted[i] && strs[i][col] > strs[i + 1][col]) {
+                needDelete = true;
+                break;
+            }
+        }
+
+        if (needDelete) {
+            deletions++;
+            continue;
+        }
+
+        // Update sorted pairs
+        for (let i = 0; i < n - 1; i++) {
+            if (!sorted[i] && strs[i][col] < strs[i + 1][col]) {
+                sorted[i] = true;
+            }
+        }
+    }
+
+    return deletions;
 };
 
 
