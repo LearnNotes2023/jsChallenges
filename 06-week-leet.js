f4bdfda18a33c5a4c2b4f2d8205008e12f3e5ec4
XenaSit <sitnikovaxena@gmail.com>
2025-12-23 04:41:28 -0500

    LEET, branch:leet, the DAILY challenge: 9. Palindrome Number

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -39955,7 +39955,16 @@ console.log("==========================================")
 // @return {boolean}
 
 var isPalindrome = function(x) {
-    
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+    let reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
 };
 
 console.log("==========================================")
