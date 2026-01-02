e91360da4161413c175fac3ca05772aebbed9863
XenaSit <sitnikovaxena@gmail.com>
2026-01-02 07:35:42 -0500

    LEET, branch:leet, the DAILY challenge: 961. N-Repeated Element in Size 2N Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -40572,7 +40572,29 @@ var plusOne = function(digits) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+961. N-Repeated Element in Size 2N Array
+Easy
+You are given an integer array nums with the following properties:
+nums.length == 2 * n.
+nums contains n + 1 unique elements.
+Exactly one element of nums is repeated n times.
+Return the element that is repeated n times.
+
+Example 1:
+Input: nums = [1,2,3,3]
+Output: 3
+
+Example 2:
+Input: nums = [2,1,2,5,3,2]
+Output: 2
+
+Example 3:
+Input: nums
+
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
