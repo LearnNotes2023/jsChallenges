36c850959233b03d94288bbcd5a799b2b916aa4c
XenaSit <sitnikovaxena@gmail.com>
2026-01-10 05:43:30 -0500

    LEET, branch:leet, the DAILY challenge: 712. Minimum ASCII Delete Sum for Two Strings

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -41033,9 +41033,31 @@ console.log("==========================================")
 // @return {number}
 
 var minimumDeleteSum = function(s1, s2) {
-    
-};
+    const m = s1.length;
+    const n = s2.length;
 
+    // dp[i][j] = max ASCII sum of common subsequence
+    // between s1[0..i-1] and s2[0..j-1]
+    const dp = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(0)
+    );
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + s1.charCodeAt(i - 1);
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    let sum1 = 0, sum2 = 0;
+    for (let c of s1) sum1 += c.charCodeAt(0);
+    for (let c of s2) sum2 += c.charCodeAt(0);
+
+    return sum1 + sum2 - 2 * dp[m][n];
+};
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
