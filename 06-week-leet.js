c4e6f549daa97b936f0bcf842b2983cfb4e3c398
XenaSit <sitnikovaxena@gmail.com>
2026-01-24 04:55:00 -0500

    LEET, branch:leet, the DAILY challenge: 1877. Minimize Maximum Pair Sum in Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -42006,7 +42006,19 @@ console.log("==========================================")
 // @return {number}
 
 var minPairSum = function(nums) {
+    nums.sort((a, b) => a - b);
     
+    let left = 0;
+    let right = nums.length - 1;
+    let maxSum = 0;
+    
+    while (left < right) {
+        maxSum = Math.max(maxSum, nums[left] + nums[right]);
+        left++;
+        right--;
+    }
+    
+    return maxSum;
 };
 
 console.log("==========================================")
