0a14f9aa4aa81a190a30d16933e989f218b98cce
XenaSit <sitnikovaxena@gmail.com>
2026-01-24 04:53:45 -0500

    LEET, branch:leet, the DAILY challenge: 1877. Minimize Maximum Pair Sum in Array

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -41991,18 +41991,24 @@ The maximum pair sum is minimized.
 Return the minimized maximum pair sum after optimally pairing up the elements.
 
 Example 1:
-
 Input: nums = [3,5,2,3]
 Output: 7
 Explanation: The elements can be paired up into pairs (3,3) and (5,2).
 The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
-Example 2:
 
+Example 2:
 Input: nums = [3,5,4,2,4,6]
 Output: 8
 Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
 The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minPairSum = function(nums) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
