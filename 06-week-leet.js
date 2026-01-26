fde1f11bc87b8971cd8a9a78b3a1833e829b909e
XenaSit <sitnikovaxena@gmail.com>
2026-01-26 05:59:58 -0500

    LEET, branch:leet, the DAILY challenge: 1200. Minimum Absolute Difference

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -42100,7 +42100,25 @@ console.log("==========================================")
 // @return {number[][]}
 
 var minimumAbsDifference = function(arr) {
+    arr.sort((a, b) => a - b);
     
+    let minDiff = Infinity;
+    
+    // Find minimum difference
+    for (let i = 1; i < arr.length; i++) {
+        minDiff = Math.min(minDiff, arr[i] - arr[i - 1]);
+    }
+    
+    const result = [];
+    
+    // Collect pairs with minimum difference
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] - arr[i - 1] === minDiff) {
+            result.push([arr[i - 1], arr[i]]);
+        }
+    }
+    
+    return result;
 };
 
 console.log("==========================================")
