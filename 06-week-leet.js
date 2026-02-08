b183a33f89f444f076e521a8efa133fe8409bd3c
XenaSit <sitnikovaxena@gmail.com>
2026-02-08 05:52:01 -0500

    LEET, branch:leet, the DAILY challenge: 110. Balanced Binary Tree

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -43084,7 +43084,19 @@ Example 3:
 Input: root = []
 Output: true
 
+Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
 
+@param {TreeNode} root
+@return {boolean}
+
+var isBalanced = function(root) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
