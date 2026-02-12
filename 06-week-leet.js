46252be13c1080c52b302d3eb0a010dcb7aef5eb
XenaSit <sitnikovaxena@gmail.com>
2026-02-12 04:38:58 -0500

    LEET, branch:leet, the DAILY challenge: 3713. Longest Balanced Substring I

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -43461,7 +43461,33 @@ console.log("==========================================")
 // @return {number}
 
 var longestBalanced = function(s) {
-    
+    let n = s.length;
+    let maxLen = 0;
+
+    for (let i = 0; i < n; i++) {
+        let freq = new Array(26).fill(0);
+        let distinct = 0;
+        let maxFreq = 0;
+
+        for (let j = i; j < n; j++) {
+            let index = s.charCodeAt(j) - 97;
+
+            if (freq[index] === 0) {
+                distinct++;
+            }
+
+            freq[index]++;
+            maxFreq = Math.max(maxFreq, freq[index]);
+
+            let length = j - i + 1;
+
+            if (length === distinct * maxFreq) {
+                maxLen = Math.max(maxLen, length);
+            }
+        }
+    }
+
+    return maxLen;
 };
 
 console.log("==========================================")
