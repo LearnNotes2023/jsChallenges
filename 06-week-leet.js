d3ceeeef67b897aee245be4e50ede24a7929ff68
XenaSit <sitnikovaxena@gmail.com>
2026-02-18 05:33:45 -0500

    LEET, branch:leet, the DAILY challenge: 693. Binary Number with Alternating Bits

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -43781,7 +43781,8 @@ console.log("==========================================")
 // @return {boolean}
 
 var hasAlternatingBits = function(n) {
-    
+    let x = n ^ (n >> 1);
+    return (x & (x + 1)) === 0;
 };
 
 console.log("==========================================")
