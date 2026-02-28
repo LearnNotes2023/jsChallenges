2afdf37dac8df62281735f9d819be9ad0c37b768
XenaSit <sitnikovaxena@gmail.com>
2026-02-28 05:27:19 -0500

    LEET, branch:leet, the DAILY challenge: 3666. Minimum Operations to Equalize Binary String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -44290,9 +44290,17 @@ console.log("==========================================")
 // @return {number}
 
 var concatenatedBinary = function(n) {
-    
-};
+    const MOD = 1000000007n;
+    let res = 0n;
+    let bits = 0n;
 
+    for(let i=1n; i<=BigInt(n); i++){
+        if((i & (i-1n)) === 0n) bits++;
+        res = ((res<<bits) | i) % MOD;
+    }
+
+    return Number(res);
+};
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
