a4d1af14a882247d403f27db4780ecb40d777c61
XenaSit <sitnikovaxena@gmail.com>
2026-03-05 06:26:50 -0500

    LEET, branch:leet, the DAILY challenge: 1758. Minimum Changes To Make Alternating Binary String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -44538,9 +44538,21 @@ console.log("==========================================")
 // @return {number}
 
 var minOperations = function(s) {
-    
-};
+    let startWith0 = 0;
+    let startWith1 = 0;
 
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== (i % 2 === 0 ? '0' : '1')) {
+            startWith0++;
+        }
+
+        if (s[i] !== (i % 2 === 0 ? '1' : '0')) {
+            startWith1++;
+        }
+    }
+
+    return Math.min(startWith0, startWith1);
+};
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
