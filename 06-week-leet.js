7725e6399c372067b59698e3c9a9ec090ebc6f75
XenaSit <sitnikovaxena@gmail.com>
2026-03-06 06:17:21 -0500

    LEET, branch:leet, the DAILY challenge: 1784. Check if Binary String Has at Most One Segment of Ones

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -44553,7 +44553,33 @@ var minOperations = function(s) {
 
     return Math.min(startWith0, startWith1);
 };
+
 console.log("==========================================")
+
+1784. Check if Binary String Has at Most One Segment of Ones
+Easy
+Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. 
+Otherwise, return false.
+
+Example 1:
+Input: s = "1001"
+Output: false
+Explanation: The ones do not form a contiguous segment.
+
+Example 2:
+Input: s = "110"
+Output: true
+
+console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
+// console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
