cec3624711d5addbfd2bb24b756e212abbc52477
XenaSit <sitnikovaxena@gmail.com>
2026-03-16 05:04:28 -0400

    LEET, branch:leet, the DAILY challenge: 1878. Get Biggest Three Rhombus Sums in a Grid

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -45256,7 +45256,44 @@ console.log("==========================================")
 // @return {number[]}
 
 var getBiggestThree = function(grid) {
-    
+    const m = grid.length;
+    const n = grid[0].length;
+    const sums = new Set();
+
+    for (let r = 0; r < m; r++) {
+        for (let c = 0; c < n; c++) {
+
+            // size 0 rhombus
+            sums.add(grid[r][c]);
+
+            for (let k = 1; ; k++) {
+
+                if (r + 2 * k >= m || c - k < 0 || c + k >= n) break;
+
+                let sum = 0;
+
+                // top -> right
+                for (let i = 0; i < k; i++)
+                    sum += grid[r + i][c + i];
+
+                // right -> bottom
+                for (let i = 0; i < k; i++)
+                    sum += grid[r + k + i][c + k - i];
+
+                // bottom -> left
+                for (let i = 0; i < k; i++)
+                    sum += grid[r + 2*k - i][c - i];
+
+                // left -> top
+                for (let i = 0; i < k; i++)
+                    sum += grid[r + k - i][c - k + i];
+
+                sums.add(sum);
+            }
+        }
+    }
+
+    return [...sums].sort((a,b) => b-a).slice(0,3);
 };
 
 console.log("==========================================")
