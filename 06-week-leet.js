b47d54bf56b26ad3a9df966846c50da4a8d450d2
XenaSit <sitnikovaxena@gmail.com>
2026-03-18 05:43:11 -0400

    LEET, branch:leet, the DAILY challenge: 3070. Count Submatrices with Top-Left Element and Sum Less Than k

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -45409,7 +45409,23 @@ console.log("==========================================")
 // @return {number}
 
 var countSubmatrices = function(grid, k) {
+    let m = grid.length;
+    let n = grid[0].length;
     
+    let count = 0;
+    
+    // Build prefix sum in-place (reuse grid to save space)
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i > 0) grid[i][j] += grid[i - 1][j];
+            if (j > 0) grid[i][j] += grid[i][j - 1];
+            if (i > 0 && j > 0) grid[i][j] -= grid[i - 1][j - 1];
+            
+            if (grid[i][j] <= k) count++;
+        }
+    }
+    
+    return count;
 };
 
 
