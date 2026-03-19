138232b5a39782b6f6d6fc82316dca455452ab81
XenaSit <sitnikovaxena@gmail.com>
2026-03-19 06:21:36 -0400

    LEET, branch:leet, the DAILY challenge: 3212. Count Submatrices With Equal Frequency of X and Y

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -45431,32 +45431,32 @@ var countSubmatrices = function(grid, k) {
 
 console.log("==========================================")
 
-3212. Count Submatrices With Equal Frequency of X and Y
-Medium
-Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
-grid[0][0]
-an equal frequency of 'X' and 'Y'.
-at least one 'X'.
+// 3212. Count Submatrices With Equal Frequency of X and Y
+// Medium
+// Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of submatrices that contain:
+// grid[0][0]
+// an equal frequency of 'X' and 'Y'.
+// at least one 'X'.
  
-Example 1:
-Input: grid = [["X","Y","."],["Y",".","."]]
-Output: 3
-Explanation:
+// Example 1:
+// Input: grid = [["X","Y","."],["Y",".","."]]
+// Output: 3
+// Explanation:
 
-Example 2:
-Input: grid = [["X","X"],["X","Y"]]
-Output: 0
-Explanation:
-No submatrix has an equal frequency of 'X' and 'Y'.
+// Example 2:
+// Input: grid = [["X","X"],["X","Y"]]
+// Output: 0
+// Explanation:
+// No submatrix has an equal frequency of 'X' and 'Y'.
 
-Example 3:
-Input: grid = [[".","."],[".","."]]
-Output: 0
-Explanation:
-No submatrix has at least one 'X'.
+// Example 3:
+// Input: grid = [[".","."],[".","."]]
+// Output: 0
+// Explanation:
+// No submatrix has at least one 'X'.
 
-@param {character[][]} grid
-@return {number}
+// @param {character[][]} grid
+// @return {number}
 
 var numberOfSubmatrices = function(grid) {
     
