617d457d14faf7ad9a70ae314717cf6d14e35108
XenaSit <sitnikovaxena@gmail.com>
2026-03-22 08:05:05 -0400

    LEET, branch:leet, the DAILY challenge: 1886. Determine Whether Matrix Can Be Obtained By Rotation

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -45610,31 +45610,30 @@ var reverseSubmatrix = function(grid, x, y, k) {
 
 console.log("==========================================")
 
-1886. Determine Whether Matrix Can Be Obtained By Rotation
-Easy
-Given two n x n binary matrices mat and target, return true if it is possible to make mat equal 
-to target by rotating mat in 90-degree increments, or false otherwise.
+// 1886. Determine Whether Matrix Can Be Obtained By Rotation
+// Easy
+// Given two n x n binary matrices mat and target, return true if it is possible to make mat equal 
+// to target by rotating mat in 90-degree increments, or false otherwise.
 
-Example 1:
-Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
-Output: true
-Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+// Example 1:
+// Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+// Output: true
+// Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
 
-Example 2:
-Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
-Output: false
-Explanation: It is impossible to make mat equal to target by rotating mat.
+// Example 2:
+// Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
+// Output: false
+// Explanation: It is impossible to make mat equal to target by rotating mat.
 
-Example 3:
-Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
-Output: true
-Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
+// Example 3:
+// Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
+// Output: true
+// Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
 
-/**
- * @param {number[][]} mat
- * @param {number[][]} target
- * @return {boolean}
- */
+// @param {number[][]} mat
+// @param {number[][]} target
+// @return {boolean}
+
 var findRotation = function(mat, target) {
     
 };
