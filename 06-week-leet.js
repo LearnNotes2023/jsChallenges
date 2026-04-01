2d9695bbbbd5640bfc2ea4322e57ee2d18b7d8b2
XenaSit <sitnikovaxena@gmail.com>
2026-04-01 06:14:28 -0400

    LEET, branch:leet, the DAILY challenge: 23. Merge k Sorted Lists

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -46402,8 +46402,81 @@ console.log("==========================================")
 // @param {ListNode[]} lists
 // @return {ListNode}
 
+class MyHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(node) {
+        this.heap.push(node);
+        this.bubbleUp();
+    }
+
+    pop() {
+        if (this.heap.length === 1) return this.heap.pop();
+        const top = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return top;
+    }
+
+    bubbleUp() {
+        let i = this.heap.length - 1;
+        while (i > 0) {
+            let parent = Math.floor((i - 1) / 2);
+            if (this.heap[parent].val <= this.heap[i].val) break;
+            [this.heap[parent], this.heap[i]] = [this.heap[i], this.heap[parent]];
+            i = parent;
+        }
+    }
+
+    bubbleDown() {
+        let i = 0;
+        const n = this.heap.length;
+
+        while (true) {
+            let left = 2 * i + 1;
+            let right = 2 * i + 2;
+            let smallest = i;
+
+            if (left < n && this.heap[left].val < this.heap[smallest].val) {
+                smallest = left;
+            }
+            if (right < n && this.heap[right].val < this.heap[smallest].val) {
+                smallest = right;
+            }
+
+            if (smallest === i) break;
+
+            [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+            i = smallest;
+        }
+    }
+
+    size() {
+        return this.heap.length;
+    }
+}
+
 var mergeKLists = function(lists) {
-    
+    const heap = new MyHeap();
+
+    for (let list of lists) {
+        if (list) heap.push(list);
+    }
+
+    let dummy = new ListNode(0);
+    let curr = dummy;
+
+    while (heap.size() > 0) {
+        let node = heap.pop();
+        curr.next = node;
+        curr = curr.next;
+
+        if (node.next) heap.push(node.next);
+    }
+
+    return dummy.next;
 };
 
 
