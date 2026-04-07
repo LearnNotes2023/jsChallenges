f114429be387a171872cc360a759a9daf673da58
XenaSit <sitnikovaxena@gmail.com>
2026-04-07 06:01:17 -0400

    LEET, branch:leet, the DAILY challenge: 2069. Walking Robot Simulation II

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -46930,43 +46930,52 @@ console.log("==========================================")
 // robot.getPos(); // return [1, 2]
 // robot.getDir(); // return "West"
 
-/**
- * @param {number} width
- * @param {number} height
- */
-var Robot = function(width, height) {
-    
+var Robot = function (width, height) {
+    this.moved = false;
+    this.idx = 0;
+    this.pos = [];
+    this.dir = [];
+    this.toDir = {
+        0: "East",
+        1: "North",
+        2: "West",
+        3: "South",
+    };
+
+    for (let i = 0; i < width; ++i) {
+        this.pos.push([i, 0]);
+        this.dir.push(0);
+    }
+    for (let i = 1; i < height; ++i) {
+        this.pos.push([width - 1, i]);
+        this.dir.push(1);
+    }
+    for (let i = width - 2; i >= 0; --i) {
+        this.pos.push([i, height - 1]);
+        this.dir.push(2);
+    }
+    for (let i = height - 2; i > 0; --i) {
+        this.pos.push([0, i]);
+        this.dir.push(3);
+    }
+    this.dir[0] = 3;
 };
 
-/** 
- * @param {number} num
- * @return {void}
- */
-Robot.prototype.step = function(num) {
-    
+Robot.prototype.step = function (num) {
+    this.moved = true;
+    this.idx = (this.idx + num) % this.pos.length;
 };
 
-/**
- * @return {number[]}
- */
-Robot.prototype.getPos = function() {
-    
+Robot.prototype.getPos = function () {
+    return this.pos[this.idx];
 };
 
-/**
- * @return {string}
- */
-Robot.prototype.getDir = function() {
-    
+Robot.prototype.getDir = function () {
+    if (!this.moved) {
+        return "East";
+    }
+    return this.toDir[this.dir[this.idx]];
 };
-
-/** 
- * Your Robot object will be instantiated and called as such:
- * var obj = new Robot(width, height)
- * obj.step(num)
- * var param_2 = obj.getPos()
- * var param_3 = obj.getDir()
- */
 
 console.log("==========================================")
 // console.log("==========================================")
