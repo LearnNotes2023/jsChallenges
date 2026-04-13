0bcc26dbde94506430b6ed80c0e04a33528d83ad
XenaSit <sitnikovaxena@gmail.com>
2026-04-13 05:38:25 -0400

    LEET, branch:leet, the DAILY challenge: 1848. Minimum Distance to the Target Element

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -47316,44 +47316,33 @@ var minimumDistance = function(word) {
 
 console.log("==========================================")
 
-1848. Minimum Distance to the Target Element
-Solved
-Easy
-Topics
-premium lock icon
-Companies
-Hint
-Given an integer array nums (0-indexed) and two integers target and start, 
-find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+// 1848. Minimum Distance to the Target Element
+// Solved
+// Given an integer array nums (0-indexed) and two integers target and start, 
+// find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+// Return abs(i - start).
+// It is guaranteed that target exists in nums.
 
-Return abs(i - start).
+// Example 1:
+// Input: nums = [1,2,3,4,5], target = 5, start = 3
+// Output: 1
+// Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.
 
-It is guaranteed that target exists in nums.
+// Example 2:
+// Input: nums = [1], target = 1, start = 0
+// Output: 0
+// Explanation: nums[0] = 1 is the only value equal to target, so the answer is abs(0 - 0) = 0.
 
+// Example 3:
+// Input: nums = [1,1,1,1,1,1,1,1,1,1], target = 1, start = 0
+// Output: 0
+// Explanation: Every value of nums is 1, but nums[0] minimizes abs(i - start), which is abs(0 - 0) = 0.
+
+// @param {number[]} nums
+// @param {number} target
+// @param {number} start
+// @return {number}
  
-
-Example 1:
-
-Input: nums = [1,2,3,4,5], target = 5, start = 3
-Output: 1
-Explanation: nums[4] = 5 is the only value equal to target, so the answer is abs(4 - 3) = 1.
-Example 2:
-
-Input: nums = [1], target = 1, start = 0
-Output: 0
-Explanation: nums[0] = 1 is the only value equal to target, so the answer is abs(0 - 0) = 0.
-Example 3:
-
-Input: nums = [1,1,1,1,1,1,1,1,1,1], target = 1, start = 0
-Output: 0
-Explanation: Every value of nums is 1, but nums[0] minimizes abs(i - start), which is abs(0 - 0) = 0.
-
-/**
- * @param {number[]} nums
- * @param {number} target
- * @param {number} start
- * @return {number}
- */
 
 console.log("==========================================")
 // console.log("==========================================")
