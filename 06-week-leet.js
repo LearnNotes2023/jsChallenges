50f054dfd38cf1f657f9bb044ef6139c5266e896
XenaSit <sitnikovaxena@gmail.com>
2026-05-01 06:51:13 -0400

    LEET, branch:leet, the DAILY challenge: 396. Rotate Function

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -48588,8 +48588,26 @@ console.log("==========================================")
  * @param {number[]} nums
  * @return {number}
  */
+
 var maxRotateFunction = function(nums) {
+    let n = nums.length;
     
+    let sum = 0;
+    let F = 0;
+    
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+        F += i * nums[i];
+    }
+    
+    let max = F;
+    
+    for (let k = 1; k < n; k++) {
+        F = F + sum - n * nums[n - k];
+        max = Math.max(max, F);
+    }
+    
+    return max;
 };
 
 // console.log("==========================================")
