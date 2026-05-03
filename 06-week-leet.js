2324c1973d22eddc8f1d3472e7c38cf7f6dc4eb8
XenaSit <sitnikovaxena@gmail.com>
2026-05-03 09:35:37 -0400

    LEET, branch:leet, the DAILY challenge: 27. Remove Element

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -48708,7 +48708,16 @@ console.log("==========================================")
 // @return {number}
 
 var removeElement = function(nums, val) {
+    let k = 0; // position to place next valid element
     
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    
+    return k;
 };
 
 console.log("==========================================")
