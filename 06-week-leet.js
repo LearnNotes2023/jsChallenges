447991615cbc27c73b7073a6be40b8529115567a
XenaSit <sitnikovaxena@gmail.com>
2026-05-04 06:40:42 -0400

    LEET, branch:leet, the DAILY challenge: 48. Rotate Image

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -48740,7 +48740,19 @@ console.log("==========================================")
 // @return {void} Do not return anything, modify matrix in-place instead.
 
 var rotate = function(matrix) {
-    
+    const n = matrix.length;
+
+    // Step 1: transpose
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Step 2: reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
 };
 
 console.log("==========================================")
