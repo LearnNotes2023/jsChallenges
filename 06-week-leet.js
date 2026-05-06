fe023ae67d2e3e6f112d25fb4935e1207625bdce
XenaSit <sitnikovaxena@gmail.com>
2026-05-06 06:33:23 -0400

    LEET, branch:leet, the DAILY challenge: 28. Find the Index of the First Occurrence in a String

--- 06-week-leet.js
+++ 06-week-leet.js
@@ -48836,9 +48836,22 @@ console.log("==========================================")
 // @return {number}
 
 var strStr = function(haystack, needle) {
-    
-};
+    if (needle === "") return 0;
 
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let j = 0;
+
+        while (j < needle.length && haystack[i + j] === needle[j]) {
+            j++;
+        }
+
+        if (j === needle.length) {
+            return i;
+        }
+    }
+
+    return -1;
+};
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
