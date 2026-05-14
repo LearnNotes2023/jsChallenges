a64ab6553c594104b9596df52a4d45bbe80652a2
XenaSit <sitnikovaxena@gmail.com>
2026-05-14 05:51:30 -0400

    LEET, branch:leet, the DAILY challenge: 2784. Check if Array is Good

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -167,7 +167,20 @@ console.log("==========================================")
 // @return {boolean}
 
 var isGood = function(nums) {
-    
+    nums.sort((a, b) => a - b);
+
+    let n = nums[nums.length - 1];
+
+    // Length must be n + 1
+    if (nums.length !== n + 1) return false;
+
+    // Check 1 to n-1
+    for (let i = 0; i < n - 1; i++) {
+        if (nums[i] !== i + 1) return false;
+    }
+
+    // Last two must both be n
+    return nums[n - 1] === n && nums[n] === n;
 };
 
 console.log("==========================================")
