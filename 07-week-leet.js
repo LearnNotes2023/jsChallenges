cb5e4b0b855be46617f80c7cbcb5d8d0d3db6982
XenaSit <sitnikovaxena@gmail.com>
2026-05-19 06:24:44 -0400

    LEET, branch:leet, the DAILY challenge: 29. Divide Two Integers

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -442,7 +442,38 @@ console.log("==========================================")
 // @return {number}
 
 var divide = function(dividend, divisor) {
-    
+    const INT_MAX = 2147483647;
+    const INT_MIN = -2147483648;
+
+    // Overflow case
+    if (dividend === INT_MIN && divisor === -1) {
+        return INT_MAX;
+    }
+
+    // Determine sign
+    const negative = (dividend < 0) !== (divisor < 0);
+
+    // Convert to positive
+    let dvd = Math.abs(dividend);
+    let dvs = Math.abs(divisor);
+
+    let result = 0;
+
+    while (dvd >= dvs) {
+        let temp = dvs;
+        let multiple = 1;
+
+        // Double using addition instead of <<
+        while (dvd >= temp + temp) {
+            temp += temp;
+            multiple += multiple;
+        }
+
+        dvd -= temp;
+        result += multiple;
+    }
+
+    return negative ? -result : result;
 };
 
 console.log("==========================================")
