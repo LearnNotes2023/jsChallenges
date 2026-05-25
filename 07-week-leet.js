d25aefaf488a7e34d5c30ed92d73c6b215667d8f
XenaSit <sitnikovaxena@gmail.com>
2026-05-25 12:13:34 -0400

    LEET, branch:leet, the DAILY challenge: 1871. Jump Game VII

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -826,30 +826,30 @@ var maxJumps = function(arr, d) {
 
 console.log("==========================================")
 
-1871. Jump Game VII
-Medium
-You are given a 0-indexed binary string s and two integers minJump and maxJump. 
-In the beginning, you are standing at index 0, which is equal to '0'. 
-You can move from index i to index j if the following conditions are fulfilled:
-i + minJump <= j <= min(i + maxJump, s.length - 1), and
-s[j] == '0'.
-Return true if you can reach index s.length - 1 in s, or false otherwise.
+// 1871. Jump Game VII
+// Medium
+// You are given a 0-indexed binary string s and two integers minJump and maxJump. 
+// In the beginning, you are standing at index 0, which is equal to '0'. 
+// You can move from index i to index j if the following conditions are fulfilled:
+// i + minJump <= j <= min(i + maxJump, s.length - 1), and
+// s[j] == '0'.
+// Return true if you can reach index s.length - 1 in s, or false otherwise.
 
-Example 1:
-Input: s = "011010", minJump = 2, maxJump = 3
-Output: true
-Explanation:
-In the first step, move from index 0 to index 3. 
-In the second step, move from index 3 to index 5.
+// Example 1:
+// Input: s = "011010", minJump = 2, maxJump = 3
+// Output: true
+// Explanation:
+// In the first step, move from index 0 to index 3. 
+// In the second step, move from index 3 to index 5.
 
-Example 2:
-Input: s = "01101110", minJump = 2, maxJump = 3
-Output: false
+// Example 2:
+// Input: s = "01101110", minJump = 2, maxJump = 3
+// Output: false
 
-@param {string} s
-@param {number} minJump
-@param {number} maxJump
-@return {boolean}
+// @param {string} s
+// @param {number} minJump
+// @param {number} maxJump
+// @return {boolean}
 
 var canReach = function(s, minJump, maxJump) {
     
