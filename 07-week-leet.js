6b57365c4da43a3c4746df96454d80be66d8d1de
XenaSit <sitnikovaxena@gmail.com>
2026-05-27 04:48:29 -0400

    LEET, branch:leet, the DAILY challenge: 3121. Count the Number of Special Characters II

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -933,32 +933,32 @@ var numberOfSpecialChars = function(word) {
 
 console.log("==========================================")
 
-3121. Count the Number of Special Characters II
-Medium
-You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, 
-and every lowercase occurrence of c appears before the first uppercase occurrence of c.
-Return the number of special letters in word.
+// 3121. Count the Number of Special Characters II
+// Medium
+// You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, 
+// and every lowercase occurrence of c appears before the first uppercase occurrence of c.
+// Return the number of special letters in word.
 
-Example 1:
-Input: word = "aaAbcBC"
-Output: 3
-Explanation:
-The special characters are 'a', 'b', and 'c'.
+// Example 1:
+// Input: word = "aaAbcBC"
+// Output: 3
+// Explanation:
+// The special characters are 'a', 'b', and 'c'.
 
-Example 2:
-Input: word = "abc"
-Output: 0
-Explanation:
-There are no special characters in word.
+// Example 2:
+// Input: word = "abc"
+// Output: 0
+// Explanation:
+// There are no special characters in word.
 
-Example 3:
-Input: word = "AbBCab"
-Output: 0
-Explanation:
-There are no special characters in word.
+// Example 3:
+// Input: word = "AbBCab"
+// Output: 0
+// Explanation:
+// There are no special characters in word.
  
-@param {string} word
-@return {number}
+// @param {string} word
+// @return {number}
 
 var numberOfSpecialChars = function(word) {
     
