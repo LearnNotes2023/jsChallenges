12b3f07c1d6fc1ebee051748e34efe64e4b4a8ba
XenaSit <sitnikovaxena@gmail.com>
2026-05-29 04:56:53 -0400

    LEET, branch:leet, the DAILY challenge: 3300. Minimum Element After Replacement With Digit Sum

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -1123,7 +1123,20 @@ console.log("==========================================")
 // @return {number}
 
 var minElement = function(nums) {
-    
+    let min = Infinity;
+
+    for (let num of nums) {
+        let sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num = Math.floor(num / 10);
+        }
+
+        min = Math.min(min, sum);
+    }
+
+    return min;
 };
 
 console.log("==========================================")
