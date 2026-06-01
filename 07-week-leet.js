5c454e123b2581a3fe2d90a71a228d0b8d6373f3
XenaSit <sitnikovaxena@gmail.com>
2026-06-01 05:02:14 -0400

    LEET, branch:leet, the DAILY challenge: 2144. Minimum Cost of Buying Candies With Discount

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -1345,10 +1345,19 @@ console.log("==========================================")
 // @return {number}
 
 var minimumCost = function(cost) {
-    
+    cost.sort((a, b) => b - a);
+
+    let total = 0;
+
+    for (let i = 0; i < cost.length; i++) {
+        // every 3rd candy is free
+        if ((i + 1) % 3 !== 0) {
+            total += cost[i];
+        }
+    }
+
+    return total;
 };
-
-
 console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
