d14ec5ab65fe282a72c9e2a28f3a7c3990450388
XenaSit <sitnikovaxena@gmail.com>
2026-06-12 05:21:29 -0400

    LEET, branch:leet, the DAILY challenge: 3559. Number of Ways to Assign Edge Weights II

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -2313,9 +2313,90 @@ console.log("==========================================")
 // @return {number[]}
 
 var assignEdgeWeights = function(edges, queries) {
-    
-};
+    const MOD = 1000000007n;
+    const n = edges.length + 1;
 
+    const graph = Array.from({ length: n + 1 }, () => []);
+
+    for (const [u, v] of edges) {
+        graph[u].push(v);
+        graph[v].push(u);
+    }
+
+    const LOG = Math.ceil(Math.log2(n)) + 1;
+
+    const depth = Array(n + 1).fill(0);
+    const up = Array.from({ length: LOG }, () => Array(n + 1).fill(0));
+
+    // DFS/BFS from root 1
+    const stack = [[1, 0]];
+    while (stack.length) {
+        const [node, parent] = stack.pop();
+
+        up[0][node] = parent;
+
+        for (const nei of graph[node]) {
+            if (nei === parent) continue;
+            depth[nei] = depth[node] + 1;
+            stack.push([nei, node]);
+        }
+    }
+
+    // Binary lifting table
+    for (let j = 1; j < LOG; j++) {
+        for (let v = 1; v <= n; v++) {
+            up[j][v] = up[j - 1][up[j - 1][v]];
+        }
+    }
+
+    function lca(a, b) {
+        if (depth[a] < depth[b]) {
+            [a, b] = [b, a];
+        }
+
+        let diff = depth[a] - depth[b];
+
+        for (let j = 0; j < LOG; j++) {
+            if ((diff >> j) & 1) {
+                a = up[j][a];
+            }
+        }
+
+        if (a === b) return a;
+
+        for (let j = LOG - 1; j >= 0; j--) {
+            if (up[j][a] !== up[j][b]) {
+                a = up[j][a];
+                b = up[j][b];
+            }
+        }
+
+        return up[0][a];
+    }
+
+    // Precompute powers of 2
+    const pow2 = Array(n).fill(0n);
+    pow2[0] = 1n;
+
+    for (let i = 1; i < n; i++) {
+        pow2[i] = (pow2[i - 1] * 2n) % MOD;
+    }
+
+    const ans = [];
+
+    for (const [u, v] of queries) {
+        const p = lca(u, v);
+        const len = depth[u] + depth[v] - 2 * depth[p];
+
+        if (len === 0) {
+            ans.push(0);
+        } else {
+            ans.push(Number(pow2[len - 1]));
+        }
+    }
+
+    return ans;
+};
 
 console.log("==========================================")
 // console.log("==========================================")
