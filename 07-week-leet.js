154f0a1dd0131ec4ca5ad9cce79ee61f5ae9b015
XenaSit <sitnikovaxena@gmail.com>
2026-06-17 23:12:02 -0400

    LEET, branch:leet, the DAILY challenge: 1344. Angle Between Hands of a Clock

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -2680,27 +2680,27 @@ var processStr = function(s) {
 
 console.log("==========================================")
 
-1344. Angle Between Hands of a Clock
-Medium
-Given two numbers, hour and minutes, return the smaller angle (in degrees) 
-formed between the hour and the minute hand.
-Answers within 10-5 of the actual value will be accepted as correct.
+// 1344. Angle Between Hands of a Clock
+// Medium
+// Given two numbers, hour and minutes, return the smaller angle (in degrees) 
+// formed between the hour and the minute hand.
+// Answers within 10-5 of the actual value will be accepted as correct.
 
-Example 1:
-Input: hour = 12, minutes = 30
-Output: 165
+// Example 1:
+// Input: hour = 12, minutes = 30
+// Output: 165
 
-Example 2:
-Input: hour = 3, minutes = 30
-Output: 75
+// Example 2:
+// Input: hour = 3, minutes = 30
+// Output: 75
 
-Example 3:
-Input: hour = 3, minutes = 15
-Output: 7.5
+// Example 3:
+// Input: hour = 3, minutes = 15
+// Output: 7.5
  
-@param {number} hour
-@param {number} minutes
-@return {number}
+// @param {number} hour
+// @param {number} minutes
+// @return {number}
 
 var angleClock = function(hour, minutes) {
     
