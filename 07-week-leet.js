dd2ff25354e3cd6754e52f7b9d520d5be04dedb5
XenaSit <sitnikovaxena@gmail.com>
2026-06-19 05:09:06 -0400

    LEET, branch:leet, the DAILY challenge: 1732. Find the Highest Altitude

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -2777,9 +2777,16 @@ console.log("==========================================")
 // @return {number}
 
 var largestAltitude = function(gain) {
-    
-};
+    let altitude = 0;
+    let maxAltitude = 0;
 
+    for (let g of gain) {
+        altitude += g;
+        maxAltitude = Math.max(maxAltitude, altitude);
+    }
+
+    return maxAltitude;
+};
 
 console.log("==========================================")
 // console.log("==========================================")
