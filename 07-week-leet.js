27aabeb296187c67a8c7440bce7bbee390803151
XenaSit <sitnikovaxena@gmail.com>
2026-06-22 05:03:28 -0400

    LEET, branch:leet, the DAILY challenge: 1189. Maximum Number of Balloons

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -2931,26 +2931,26 @@ var maxIceCream = function(costs, coins) {
 
 console.log("==========================================")
 
-1189. Maximum Number of Balloons
-Easy
-Given a string text, 
-you want to use the characters of text to form as many instances of the word "balloon" as possible.
-You can use each character in text at most once. Return the maximum number of instances that can be formed.
+// 1189. Maximum Number of Balloons
+// Easy
+// Given a string text, 
+// you want to use the characters of text to form as many instances of the word "balloon" as possible.
+// You can use each character in text at most once. Return the maximum number of instances that can be formed.
 
-Example 1:
-Input: text = "nlaebolko"
-Output: 1
+// Example 1:
+// Input: text = "nlaebolko"
+// Output: 1
 
-Example 2:
-Input: text = "loonbalxballpoon"
-Output: 2
+// Example 2:
+// Input: text = "loonbalxballpoon"
+// Output: 2
 
-Example 3:
-Input: text = "leetcode"
-Output: 0
+// Example 3:
+// Input: text = "leetcode"
+// Output: 0
 
-@param {string} text
-@return {number}
+// @param {string} text
+// @return {number}
 
 var maxNumberOfBalloons = function(text) {
     
