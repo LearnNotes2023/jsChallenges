e6c2b0ab14fd862949a7fddc526d2c768c5b2dd2
XenaSit <sitnikovaxena@gmail.com>
2026-06-22 05:04:02 -0400

    LEET, branch:leet, the DAILY challenge: 1189. Maximum Number of Balloons

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -2953,7 +2953,19 @@ console.log("==========================================")
 // @return {number}
 
 var maxNumberOfBalloons = function(text) {
-    
+    const count = {};
+
+    for (const char of text) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    return Math.min(
+        count['b'] || 0,
+        count['a'] || 0,
+        Math.floor((count['l'] || 0) / 2),
+        Math.floor((count['o'] || 0) / 2),
+        count['n'] || 0
+    );
 };
 
 console.log("==========================================")
