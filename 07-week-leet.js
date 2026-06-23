584eb95a15716570ba7ed83f68c3f01804a88c27
XenaSit <sitnikovaxena@gmail.com>
2026-06-23 04:51:41 -0400

    LEET, branch:leet, the DAILY challenge: 3699. Number of ZigZag Arrays I

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -3002,6 +3002,15 @@ There are 10 valid ZigZag arrays of length n = 3 using values in the range [1, 3
 [3, 1, 2], [3, 1, 3], [3, 2, 3]
 All arrays meet the ZigZag conditions.
 
+/**
+ * @param {number} n
+ * @param {number} l
+ * @param {number} r
+ * @return {number}
+ */
+var zigZagArrays = function(n, l, r) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
