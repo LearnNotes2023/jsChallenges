2a3b8e48777d801f96b4d8efd76db02508d52c0c
XenaSit <sitnikovaxena@gmail.com>
2026-07-05 11:17:20 -0400

    LEET, branch:leet, the DAILY challenge: 1301. Number of Paths with Max Score

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -3885,22 +3885,24 @@ Return a list of two integers: the first integer is the maximum sum of numeric c
 and the second is the number of such paths that you can take to get that maximum sum, taken modulo 10^9 + 7.
 In case there is no path, return [0, 0].
 
- 
-
 Example 1:
-
 Input: board = ["E23","2X2","12S"]
 Output: [7,1]
-Example 2:
 
+Example 2:
 Input: board = ["E12","1X1","21S"]
 Output: [4,2]
-Example 3:
 
+Example 3:
 Input: board = ["E11","XXX","11S"]
 Output: [0,0]
 
+@param {string[]} board
+@return {number[]}
 
+var pathsWithMaxScore = function(board) {
+    
+};
 
 console.log("==========================================")
 // console.log("==========================================")
