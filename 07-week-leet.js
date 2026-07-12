c924c2d03503450ff8c8eb7e7b8def0f6de9c74f
XenaSit <sitnikovaxena@gmail.com>
2026-07-11 22:37:13 -0400

    LEET, branch:leet, the DAILY challenge: 44. Wildcard Matching

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -4370,7 +4370,31 @@ var pathExistenceQueries = function(n, nums, maxDiff, queries) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+44. Wildcard Matching
+Hard
+Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Example 1:
+Input: s = "aa", p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input: s = "aa", p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+
+Example 3:
+Input: s = "cb", p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
