835f724a69ad5eccdfd414398fe42f44ff8171dd
XenaSit <sitnikovaxena@gmail.com>
2026-07-14 05:22:41 -0400

    LEET, branch:leet, the DAILY challenge: 3336. Find the Number of Subsequences With Equal GCD

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -4554,8 +4554,50 @@ console.log("==========================================")
 // @param {number[]} nums
 // @return {number}
 
-var subsequencePairCount = function(nums) {
-    
+var subsequencePairCount = function (nums) {
+    const MOD = 1000000007;
+    const m = Math.max(...nums);
+
+    const gcd = (a, b) => {
+        while (b !== 0) {
+            [a, b] = [b, a % b];
+        }
+        return a;
+    };
+
+    let dp = Array.from({ length: m + 1 }, () => new Array(m + 1).fill(0));
+    dp[0][0] = 1;
+
+    for (const num of nums) {
+        const ndp = Array.from({ length: m + 1 }, () =>
+            new Array(m + 1).fill(0),
+        );
+
+        for (let j = 0; j <= m; j++) {
+            const divisor1 = gcd(j, num);
+            const dpRow = dp[j];
+            const ndpRow = ndp[j];
+            const ndpD1Row = ndp[divisor1];
+
+            for (let k = 0; k <= m; k++) {
+                const val = dpRow[k];
+                if (val === 0) continue;
+
+                const divisor2 = gcd(k, num);
+                ndpRow[k] = (ndpRow[k] + val) % MOD;
+                ndpD1Row[k] = (ndpD1Row[k] + val) % MOD;
+                ndpRow[divisor2] = (ndpRow[divisor2] + val) % MOD;
+            }
+        }
+        dp = ndp;
+    }
+
+    let ans = 0;
+    for (let j = 1; j <= m; j++) {
+        ans = (ans + dp[j][j]) % MOD;
+    }
+
+    return ans;
 };
 
 console.log("==========================================")
