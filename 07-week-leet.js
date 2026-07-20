50df262b7ae1b450310089b675ecefc485999e1a
XenaSit <sitnikovaxena@gmail.com>
2026-07-20 05:04:07 -0400

    LEET, branch:leet, the DAILY challenge: 46. Permutations

--- 07-week-leet.js
+++ 07-week-leet.js
@@ -4897,7 +4897,31 @@ var shiftGrid = function(grid, k) {
 };
 
 console.log("==========================================")
-// console.log("==========================================")
+
+46. Permutations
+Medium
+Topics
+premium lock icon
+Companies
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+
+console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
 // console.log("==========================================")
